0e16b7ddf35422273403c1ad02003033b35a6831
Matias Santillan <sntlln.93@gmail.com>
2021-07-18 09:35:37 -0300

    added quotas table to avoid calculate interest increase

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    
+    protected $guarded = [];
+
+    public function quotas()
+    {
+        return $this->hasMany(Quota::class);
+    }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\Quota;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -14,9 +15,48 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        Product::create([
-            'price' => 3600.00,
+        $product = Product::create([
             'name' => 'Clásico'
+        ]);
+
+        Quota::create([
+            'quantity' => 1,
+            'quota_amount' => 3000,
+            'product_id' => $product->id
+        ]);
+
+        Quota::create([
+            'quantity' => 2,
+            'quota_amount' => 1600,
+            'product_id' => $product->id
+        ]);
+
+        Quota::create([
+            'quantity' => 3,
+            'quota_amount' => 1400,
+            'product_id' => $product->id
+        ]);
+
+        $product = Product::create([
+            'name' => 'Moderno'
+        ]);
+
+        Quota::create([
+            'quantity' => 1,
+            'quota_amount' => 3000,
+            'product_id' => $product->id
+        ]);
+
+        Quota::create([
+            'quantity' => 2,
+            'quota_amount' => 1600,
+            'product_id' => $product->id
+        ]);
+
+        Quota::create([
+            'quantity' => 3,
+            'quota_amount' => 1400,
+            'product_id' => $product->id
         ]);
     }
 }
