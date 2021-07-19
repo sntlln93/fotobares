dbf4572cfe8457f8d2277bded440de0b55a6a1ce
Matias Santillan <sntlln.93@gmail.com>
2021-07-19 00:50:43 -0300

    changed quota amount

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -43,13 +43,13 @@ class ProductSeeder extends Seeder
 
         Quota::create([
             'quantity' => 1,
-            'quota_amount' => 3000,
+            'quota_amount' => 4000,
             'product_id' => $product->id
         ]);
 
         Quota::create([
             'quantity' => 2,
-            'quota_amount' => 1600,
+            'quota_amount' => 2100,
             'product_id' => $product->id
         ]);
 
