8a5b9fd87c41ab93f38ada8ccd433986d1d14174
Matias Santillan <sntlln.93@gmail.com>
2021-07-26 00:33:23 -0300

    fixed layout

--- resources/views/products/_form.blade.php
+++ resources/views/products/_form.blade.php
@@ -13,51 +13,54 @@
 
 <br>
 <h1 class="h4 text-gray-800">Valor de las cuotas</h1>
-<table class="table table-responsive table-bordered">
-    <thead>
-        <tr>
-            <th>Cantidad de cuotas</th>
-            <th>Valor por cuota</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse (old('quotas', $product->quotas) as $quota)
-        <tr>
-            <td>
-                <input class="form-control @error('quotas[{{ $loop->index }}][quantity]') is-invalid @enderror"
-                    type="number" readonly value="{{ $loop->iteration }}" name="quotas[{{ $loop->index }}][quantity]">
-            </td>
-            <td>
-                <input type="number"
-                    class="form-control @error('quotas[{{ $loop->index }}][quota_amount]') is-invalid @enderror"
-                    value="{{ is_array($quota) ? $quota['quota_amount'] : $quota->quota_amount }}"
-                    name="quotas[{{ $loop->index }}][quota_amount]">
-            </td>
-        </tr>
-        @empty
-        <tr>
-            <td>
-                <input class="form-control @error('quotas[0][quantity]') is-invalid @enderror" type="number" readonly
-                    value="{{ $product->name }}" name="quotas[0][quantity]">
-            </td>
-            <td>
-                <input type="number" class="form-control @error('quotas[0][quota_amount]') is-invalid @enderror"
-                    name="quotas[0][quota_amount]">
-            </td>
-        </tr>
-        @endforelse
-    </tbody>
-    @if($errors->any())
-    <tfoot>
-        <tr>
-            <td colspan="3">
-                <ul class="alert alert-danger">
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </td>
-        </tr>
-    </tfoot>
-    @endif
-</table>
+<div class="table-responsive">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Cantidad de cuotas</th>
+                <th>Valor por cuota</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse (old('quotas', $product->quotas) as $quota)
+            <tr>
+                <td>
+                    <input class="form-control @error('quotas[{{ $loop->index }}][quantity]') is-invalid @enderror"
+                        type="number" readonly value="{{ $loop->iteration }}"
+                        name="quotas[{{ $loop->index }}][quantity]">
+                </td>
+                <td>
+                    <input type="number"
+                        class="form-control @error('quotas[{{ $loop->index }}][quota_amount]') is-invalid @enderror"
+                        value="{{ is_array($quota) ? $quota['quota_amount'] : $quota->quota_amount }}"
+                        name="quotas[{{ $loop->index }}][quota_amount]">
+                </td>
+            </tr>
+            @empty
+            <tr>
+                <td>
+                    <input class="form-control @error('quotas[0][quantity]') is-invalid @enderror" type="number"
+                        readonly value="{{ $product->name }}" name="quotas[0][quantity]">
+                </td>
+                <td>
+                    <input type="number" class="form-control @error('quotas[0][quota_amount]') is-invalid @enderror"
+                        name="quotas[0][quota_amount]">
+                </td>
+            </tr>
+            @endforelse
+        </tbody>
+        @if($errors->any())
+        <tfoot>
+            <tr>
+                <td colspan="3">
+                    <ul class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </td>
+            </tr>
+        </tfoot>
+        @endif
+    </table>
+</div>
