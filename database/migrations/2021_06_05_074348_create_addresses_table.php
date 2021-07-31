2de729e47ec8bb15ae425aa6d6d364bc9b650246
Matias Santillan <sntlln.93@gmail.com>
2021-07-31 10:43:48 -0300

    added city

--- database/migrations/2021_06_05_074348_create_addresses_table.php
+++ database/migrations/2021_06_05_074348_create_addresses_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('city')->default('La Rioja');
             $table->string('neighborhood');
             $table->string('street');
             $table->string('number')->nullable();
