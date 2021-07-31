8468792c12b094b9971f95ec1c675a87b4baed0b
Matias Santillan <sntlln.93@gmail.com>
2021-07-30 22:50:37 -0300

    using one controller instead of two

--- app/Http/Controllers/web/clients/ShowClients.php
+++ app/Http/Controllers/web/clients/ShowClients.php
@@ -5,8 +5,15 @@ namespace App\Http\Controllers\web\clients;
 use App\Http\Controllers\Controller;
 use App\Models\Client;
 
-class ShowClient extends Controller
+class ShowClients extends Controller
 {
+    public function index()
+    {
+        $clients = Client::all();
+
+        return view('clients.index')->with('clients', $clients);
+    }
+
     public function show(Client $client)
     {
         return view('clients.show')->with('client', $client);
