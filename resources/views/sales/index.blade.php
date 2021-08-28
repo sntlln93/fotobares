74fdde302be6361f99623d2dc9fb48b165c55466
Matias Santillan <sntlln.93@gmail.com>
2021-08-28 11:42:03 -0300

    partialized sale.index rows

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -24,22 +24,7 @@
                 </thead>
                 <tbody>
                     @foreach ($sales as $sale)
-                    <tr>
-                        <td>{{ $sale->id }}</td>
-                        <td><a
-                                href="{{ route('clients.show', ['client' => $sale->client->id]) }}">{{ $sale->client->full_name }}</a>
-                        </td>
-                        <td>{{ $sale->formatted_deliver_on }}</td>
-                        <td>{{ $sale->formatted_delivered_at }}</td>
-                        <td><a
-                                href="{{ route('employees.show', ['employee' => $sale->seller_id]) }}">{{ $sale->seller->full_name }}</a>
-                        </td>
-                        <td>{{ $sale->created_at->diffForHumans() }}</td>
-                        <td>
-                            <a href="{{ route('sales.show', ['sale' => $sale->id]) }}" class="btn btn-sm btn-info"><i
-                                    class="fas fa-eye"></i></a>
-                        </td>
-                    </tr>
+                    @include('sales._index')
                     @endforeach
                 </tbody>
                 @if($sales->hasPages())
