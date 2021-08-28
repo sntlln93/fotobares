87a90baa71580f5087dcf930b69e9f506641e666
Matias Santillan <sntlln.93@gmail.com>
2021-08-28 12:48:02 -0300

    added role name in local lang

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,4 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    public function getIsoNameAttribute()
+    {
+        if ($this->name==='admin') {
+            return 'Administrador';
+        }
+        if ($this->name==='seller') {
+            return 'Vendedor';
+        }
+    }
 }
