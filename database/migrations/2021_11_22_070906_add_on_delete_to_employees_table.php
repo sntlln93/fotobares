592ef05c5466ccdff13b458f7b4da86471323ab4
Matias Santillan <sntlln.93@gmail.com>
2021-11-22 08:02:46 -0300

    💩 🐛 bug/added on delete to avoid issues when deleting an user.

---
+++ database/migrations/2021_11_22_070906_add_on_delete_to_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+
+            DB::statement('ALTER TABLE employees MODIFY user_id bigint unsigned DEFAULT NULL');
+            
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->constrained()
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+        });
+    }
+};
