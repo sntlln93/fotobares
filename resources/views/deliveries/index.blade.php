08b4505a2915ad650b3dca1ad59101ff28d2d5de
Matias Santillan <sntlln.93@gmail.com>
2022-01-05 00:23:50 -0300

    ♻️ ✨ Resolves #70 Added filter by date range, search. Also refactored to be renderout by js instead of blade

--- resources/views/deliveries/index.blade.php
+++ resources/views/deliveries/index.blade.php
@@ -3,100 +3,256 @@
 @section('title', 'Entregas')
 
 @section('styles')
+<link rel="stylesheet" href="{{ asset('css/utils/flatpickr.min.css') }}">
 <style>
-    .card-max-width {
-        margin-block: .8em;
+    .card-grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+        grid-gap: 5px;
+
+    }
+
+    @media (max-width: 968px) {
+        .card-grid {
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+        }
+    }
+
+    @media (max-width: 768px) {
+        .card-grid {
+            grid-template-columns: repeat(1, 1fr);
+            grid-template-rows: repeat(1, 1fr);
+        }
     }
 </style>
 @endsection
 
 @section('content')
-<div class="input-group mb-2">
-    <input type="text" class="form-control" id="searchInput">
-    <div class="input-group-append">
-        <span class="input-group-text"><i class="fas fa-search"></i></span>
-    </div>
-</div>
-
 <div class="row">
-    @foreach ($sales as $sale)
-    <div class="card col-md-6 col-lg-4 card-max-width p-0">
-        <div class="card-header text-right">
-            <button type="button" class="btn btn-sm btn-primary" data-toggle="modal"
-                data-target="#photo{{ $sale->id }}"><i class="fas fa-image"></i></button>
-            <a class="btn btn-sm btn-warning" href="{{ url('map/' . $sale->client_id) }}">
-                <i class="fas fa-map-marker"></i>
-            </a>
-            @foreach ($sale->client->phones as $phone)
-            <a class="btn btn-sm btn-success" href="{{ route('whatsapp.send', ['phone' => $phone]) }}" target="_blank">
-                <i class="fab fa-whatsapp"></i>
-            </a>
-            <a href="tel:{{ $phone->full_number }}" class="btn btn-sm btn-primary"><i class="fas fa-phone"></i></a>
-            @endforeach
-            <a href="{{ route('recalculate.form', ['sale' => $sale->id]) }}" class="btn btn-sm btn-dark"><i
-                    class="fas fa-credit-card"></i></a>
-            <a href="{{ route('sales.show', ['sale' => $sale->id]) }}" class="text-uppercase btn btn-sm btn-info"><i
-                    class="fas fa-eye"></i> Ver</a>
-        </div>
-
-        <div class="modal fade" id="photo{{$sale->id}}" tabindex="-1" aria-labelledby="photo{{$sale->id}}Label"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="photo{{$sale->id}}Label">Fotos</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        @foreach ($sale->details as $detail)
-                        <img src="{{ asset('storage/'.$detail->photo?->path) }}" alt="" class="img-fluid">
-                        @endforeach
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-body">
-            <p class="mb-0">
-                <b>Cliente:</b>
-                <a href="{{ route('clients.show', ['client' => $sale->client_id]) }}">{{
-                    $sale->client->full_name }}</a>
-            </p>
-            <p class="mb-0">
-                <b>Producto:</b>
-                @foreach ($sale->details as $detail)
-                @include('_partials.detail')
-                @endforeach
-            </p>
-            <p class="mb-0">
-                <b>Vendedor:</b>
-                {{ $sale->seller->full_name }}
-            </p>
-            <p class="mb-0">
-                <b>Dirección:</b>
-                <span>
-                    {{ $sale->client->address->formatted_address }}
-                    <a class="btn btn-sm btn-warning ml-1" href="{{ url('map/' . $sale->client_id) }}">
-                        <i class="fas fa-map-marker"></i>
-                    </a>
-                </span>
-            </p>
-            <p class="mb-2">
-                <b>Fecha de entrega:</b>
-                {{ $sale->deliver_on->isoFormat('D [de] MMMM Y') }}
-            </p>
-            <a class="w-100 btn btn-primary" href="{{ route('collect', ['sale' => $sale->id]) }}">Entregar</a>
+    <div class="input-group col-sm-12 col-md-6 mb-2">
+        <input type="text" class="form-control" id="searchInput">
+        <div class="input-group-append">
+            <span class="input-group-text"><i class="fas fa-search"></i></span>
         </div>
     </div>
-    @endforeach
+    <div class="col-sm-12 col-md-6 mb-2">
+        <button id="dateRangeFilterBtn" class="btn btn-sm btn-light border h-100">
+            <span>Fechas</span>
+            <i class="fas fa-calendar-alt"></i>
+        </button>
+        <button class="btn btn-sm btn-light border h-100 clear_button" title="clear" data-clear>
+            <i class="text-danger fas fa-minus-circle"></i>
+        </button>
+    </div>
 </div>
+
+<div class="card-grid"></div>
 
 @endsection
 
 @section('scripts')
+<script src="{{ asset('js/utils/time.js') }}"></script>
+<script src="{{ asset('js/utils/flatpickr.min.js') }}"></script>
+<script src="{{ asset('js/utils/flatpickr.l10n.min.js') }}"></script>
+
+<script>
+    const sales = Object.values(@json($sales)).sort((a, b) => new Date(a.due_date) - new Date(b.due_date));
+    const grid = document.querySelector('.card-grid');
+
+    const routes = {
+        client:'{{ route('clients.show', ['client' => ':client']) }}',
+        whatsapp:'{{ route('whatsapp.send', ['phone' => ':phone']) }}',
+        map: '{{ route('map.show', ['client' => ':client']) }}',
+        images: '{{ asset('storage/'. ':path') }}',
+        sales: {
+            show: '{{ route('sales.show', ['sale' => ':sale']) }}',
+            collect: '{{ route('collect', ['sale' => ':sale']) }}',
+        },
+    }
+
+    const rendersales = (sales) => {
+        grid.innerHTML = '';
+        sales.forEach(sale => {
+            const phoneBtns = sale.client.phones.reduce((acc, phone) => {
+                return acc + `<a class="btn btn-sm btn-success ml-1" href="${ routes.whatsapp.replace(':phone', phone.id) }" target="_blank">
+                            <i class="fab fa-whatsapp"></i>
+                        </a>
+                        <a href="tel:${ phone.full_number }" class="btn btn-sm btn-primary"><i class="fas fa-phone"></i></a>`;
+            }, '');
+
+            const detailBadges = sale.details.reduce((acc, detail) => {
+                return acc + `<a href="${ routes.sales.show.replace(':sale', detail.sale_id) }">
+                    <span class="badge badge-light border--transparent">${ detail.product.name }
+                        ${ detail.description ? '(' + detail.description + ')' : '(-)'}
+                        <i class="fas fa-circle color-indicator"
+                            style="color: ${ detail.color }; background-color: ${ detail.color }">
+                        </i>
+                    </span>
+                </a>`;
+            }, '');
+
+            const card = document.createElement('div');
+            card.classList = 'card';
+            card.innerHTML = `
+                <div class="card-header text-right">
+                    <button type="button" class="btn btn-sm btn-primary" data-open="modal"
+                        data-target="#photo${ sale.id }"><i class="fas fa-image"></i></button>
+                    <a class="btn btn-sm btn-warning" href="${ routes.map.replace(':client', sale.client.id) }">
+                        <i class="fas fa-map-marker"></i>
+                    </a>
+                    ${phoneBtns}
+                    <a href="${ routes.sales.show.replace(':sale', sale.id) }" class="btn btn-sm btn-dark"><i
+                            class="fas fa-credit-card"></i></a>
+                    <a href="${ routes.sales.show.replace(':sale', sale.id) }" class="text-uppercase btn btn-sm btn-info"><i
+                            class="fas fa-eye"></i> Ver</a>
+                </div> 
+
+                <div class="card-body">
+                    <p class="mb-0">
+                        <b>Cliente:</b>
+                        <a href="${ routes.client.replace(':client', sale.client.id) }">${
+                            sale.client.full_name }</a>
+                    </p>
+                    <p class="mb-0">
+                        <b>Producto:</b>
+                        ${detailBadges}
+                    </p>
+                    <p class="mb-0">
+                        <b>Vendedor:</b>
+                        ${ sale.seller.full_name }
+                    </p>
+                    <p class="mb-0">
+                        <b>Dirección:</b>
+                        <span>
+                            ${ sale.client.address.formatted_address }
+                        </span>
+                    </p>
+                    <p class="mb-2">
+                        <b>Fecha de entrega:</b>
+                        ${ getFormattedDate(sale.deliver_on) }
+                    </p>
+                    <a class="w-100 btn btn-primary" href="${ routes.sales.collect.replace(':sale', sale.id) }">Entregar</a>
+                </div>`;
+
+            grid.appendChild(card);
+        });
+    }
+
+    rendersales(sales);
+</script>
+
+<script>
+    const filterByDateRange = (selectedDates) => {
+        const filteredsales = sales.filter(s => new Date(s.deliver_on) >= new Date(selectedDates[0]) && new Date(s.deliver_on) <= new Date(selectedDates[1]))
+        rendersales(filteredsales);
+    };
+
+    flatpickr("#dateRangeFilterBtn", {
+        "locale": "es",
+        mode: "range",
+        dateFormat: "d/M",
+        onOpen: function() {
+            document.querySelector("#dateRangeFilterBtn").removeAttribute('value');
+        },
+        onValueUpdate: function(selectedDates, dateStr) {
+            const filtersalesByDateBtn = document.querySelector("#dateRangeFilterBtn");
+
+            filtersalesByDateBtn.querySelector('span').innerText = dateStr;
+
+            filterByDateRange(selectedDates);
+            selectedDates.length && filtersalesByDateBtn.classList.add('filter--active');
+        },
+    });
+
+    //cleaning selection
+    document.querySelector('.clear_button').addEventListener('click', function() {
+        const filtersalesByDateBtn = document.querySelector("#dateRangeFilterBtn");
+
+        filtersalesByDateBtn.querySelector('span').innerText = 'Fechas';
+        filtersalesByDateBtn.classList.remove('filter--active');
+        filtersalesByDateBtn.flatpickr().clear();
+        rendersales(sales);
+    });
+
+    //search on sales
+    const searchInput = document.querySelector('#searchInput');
+  
+    const flattenObj = (obj, parent, res = {}) => {
+        for(let key in obj){
+            let propName = parent ? parent + '_' + key : key;
+            if(typeof obj[key] == 'object'){
+                flattenObj(obj[key], propName, res);
+            } else {
+                res[propName] = obj[key];
+            }
+        }
+        return res;
+    }
+
+    const searchOnsales = () => {
+        const params = searchInput.value.toLowerCase();
+        const toRender = sales.filter(payment => Object.values(flattenObj(payment)).some(p => String(p).toLowerCase().includes(params)));
+        
+        toRender.length > 0
+        ? rendersales(toRender)
+        : grid.innerHTML = 'No hay entregas que coincidan con la búsqueda';
+    }
+
+    searchInput.addEventListener('input', searchOnsales);
+</script>
+
+
+<script>
+    const openModal = (sale) => {
+        const modal = document.createElement('div');
+
+        const modalBody = sale.details.reduce((acc, detail) => {
+            return acc + `<img src="${ routes.images.replace(':path', detail?.photo?.path) }" alt="" class="img-fluid">`;
+        }, '');
+
+        modal.classList = 'modal fade';
+        modal.id = `photo${ sale.id }`;
+        modal.ariaLabelledby = 'photo${sale.id}Label';
+        modal.setAttribute('data-close-modal', modal.id);
+        modal.innerHTML = `
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="photo${sale.id}Label">Fotos</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close" data-close-modal=${modal.id}>
+                            <span aria-hidden="true" data-close-modal=${modal.id}>&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ${modalBody}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal" data-close-modal=${modal.id}>Close</button>
+                    </div>
+                </div>
+            </div>`;
+
+        document.body.appendChild(modal);
+        $(`#${modal.id}`).modal('show');
+    };
+
+    //for open and closing modals
+    document.addEventListener('click', e => {
+        const target =  e.target;
+
+        if(target.hasAttribute('data-open')){
+            const sale = sales.find(s => s.id === parseInt(target.getAttribute('data-target').replace('#photo', '')));
+            openModal(sale);
+        }
+
+        if(target.hasAttribute('data-close-modal')) {
+            console.log(target.getAttribute('data-close-modal'));
+            const toRemove = document.getElementById(target.getAttribute('data-close-modal'));
+            document.body.removeChild(toRemove);
+        }
+    });
+</script>
+
 @endsection
--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -3,70 +3,188 @@
 @section('title', 'Entregas')
 
 @section('styles')
+<link rel="stylesheet" href="{{ asset('css/utils/flatpickr.min.css') }}">
 <style>
-    .card-max-width {
-        margin-block: .8em;
+    .card-grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+        grid-gap: 5px;
+
+    }
+
+    @media (max-width: 968px) {
+        .card-grid {
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+        }
+    }
+
+    @media (max-width: 768px) {
+        .card-grid {
+            grid-template-columns: repeat(1, 1fr);
+            grid-template-rows: repeat(1, 1fr);
+        }
     }
 </style>
 @endsection
 
 @section('content')
-<div class="input-group mb-2">
-    <input type="text" class="form-control" id="searchInput">
-    <div class="input-group-append">
-        <span class="input-group-text"><i class="fas fa-search"></i></span>
+<div class="row">
+    <div class="input-group col-sm-12 col-md-6 mb-2">
+        <input type="text" class="form-control" id="searchInput">
+        <div class="input-group-append">
+            <span class="input-group-text"><i class="fas fa-search"></i></span>
+        </div>
+    </div>
+    <div class="col-sm-12 col-md-6 mb-2">
+        <button id="dateRangeFilterBtn" class="btn btn-sm btn-light border h-100">
+            <span>Fechas</span>
+            <i class="fas fa-calendar-alt"></i>
+        </button>
+        <button class="btn btn-sm btn-light border h-100 clear_button" title="clear" data-clear>
+            <i class="text-danger fas fa-minus-circle"></i>
+        </button>
     </div>
 </div>
 
-<div class="row">
-    @foreach ($payments as $payment)
-    <div class="card col-md-6 col-lg-4 card-max-width p-0">
-        <div class="card-header text-right">
-            @foreach ($payment->phones as $phone)
-            <a class="btn btn-sm btn-success ml-1" href="{{ route('whatsapp.send', ['phone' => $phone]) }}"
-                target="_blank">
-                <i class="fab fa-whatsapp"></i>
-            </a>
-            @endforeach
-            @if ($payment->client->has_location)
-            <a class="btn btn-sm btn-warning ml-1" href="{{ url('map/' . $payment->client->id) }}">
-                <i class="fas fa-map-marker"></i>
-            </a>
-            @endif
-            <a class="btn btn-sm btn-primary ml-1" href="{{ route('sales.show', ['sale' => $payment->sale_id]) }}">
-                <i class="fas fa-dollar-sign"></i>
-            </a>
-            <a class="btn btn-sm btn-info ml-1" href="{{ route('collect', ['sale' => $payment->sale_id]) }}">
-                <i class="fas fa-hand-holding-usd"></i>
-            </a>
-        </div>
-        <div class="card-body">
-            <p class="mb-0">
-                <b>Cliente:</b>
-                <a href="{{ route('clients.show', ['client'=> $payment->client->id]) }}">{{
-                    $payment->client->full_name }}</a>
-            </p>
-            <p class="mb-0"><b>Cuota: </b> ${{ $payment->amount }}</p>
-            <p class="mb-0">
-                <b>Vencimiento:</b>
-                {{ $payment->due_date->isoFormat('D [de] MMMM Y') }}
-            </p>
-            <p class="mb-0">
-                <b>Hora:</b>
-                {{ $payment->hour}}
-            </p>
-
-            <p class="mb-2">
-                <b>Dirección:</b>
-                {{ $payment->client->address }}
-            </p>
-        </div>
-
-    </div>
-    @endforeach
+<div class="card-grid">
 </div>
 
 @endsection
 
 @section('scripts')
+<script src="{{ asset('js/utils/time.js') }}"></script>
+<script src="{{ asset('js/utils/flatpickr.min.js') }}"></script>
+<script src="{{ asset('js/utils/flatpickr.l10n.min.js') }}"></script>
+
+<script>
+    const grid = document.querySelector('.card-grid');
+    const payments = Object.values(@json($payments)).sort((a, b) => new Date(a.due_date) - new Date(b.due_date));
+    const routes = {
+        client:'{{ route('clients.show', ['client' => ':client']) }}',
+        whatsapp:'{{ route('whatsapp.send', ['phone' => ':phone']) }}',
+        map: '{{ route('map.show', ['client' => ':client']) }}',
+        sales: {
+            show: '{{ route('sales.show', ['sale' => ':sale']) }}',
+            collect: '{{ route('collect', ['sale' => ':sale']) }}',
+        },
+    }
+    
+    const createBtns = (payment) => {
+        const phones = payment.phones.reduce((acc, phone) => {
+            return acc += `<a class="btn btn-sm btn-success ml-1" href="${routes.whatsapp.replace(':phone', phone)}" target="_blank"><i class="fab fa-whatsapp"></i></a>`;
+        }, '');
+
+        const mapBtn = payment.client.has_location ? `<a class="btn btn-sm btn-warning ml-1" href="${routes.map.replace(':client', payment.client.id)}"><i class="fas fa-map-marker"></i></a>` : '';
+        
+        return `
+            ${phones}
+            ${mapBtn}
+            <a class="btn btn-sm btn-primary ml-1" href="${ routes.sales.show.replace(':sale', payment.sale_id) }">
+                <i class="fas fa-dollar-sign"></i>
+            </a>
+            <a class="btn btn-sm btn-info ml-1" href="${ routes.sales.collect.replace(':sale', payment.sale_id) }">
+                <i class="fas fa-hand-holding-usd"></i>
+            </a>`;
+    }
+
+    const renderPayments = (payments) => {
+        grid.innerHTML = '';
+        payments.forEach(payment => {
+            const card = document.createElement('div');
+            card.classList = 'card';
+            card.innerHTML = `
+                <div class="card-header text-right">
+                    ${createBtns(payment)}                
+                </div>
+                <div class="card-body">
+                    <p class="mb-0">
+                        <b>Cliente:</b>
+                        <a href="${ routes.client.replace(':client', payment.client.id) }">${
+                            payment.client.full_name }</a>
+                    </p>
+                    <p class="mb-0"><b>Cuota: </b> $${ payment.amount }</p>
+                    <p class="mb-0">
+                        <b>Vencimiento:</b>
+                        ${ getFormattedDate(payment.due_date) }
+                    </p>
+                    <p class="mb-0">
+                        <b>Hora:</b>
+                        ${ payment.hour}
+                    </p>
+
+                    <p class="mb-2">
+                        <b>Dirección:</b>
+                        ${ payment.client.address }
+                    </p>
+                </div>`;
+
+            grid.appendChild(card);
+        });
+    }
+
+    renderPayments(payments);
+</script>
+
+<script>
+    const filterByDateRange = (selectedDates) => {
+        const filteredPayments = payments.filter(p => new Date(p.due_date) >= new Date(selectedDates[0]) && new Date(p.due_date) <= new Date(selectedDates[1]))
+        renderPayments(filteredPayments);
+    };
+
+    flatpickr("#dateRangeFilterBtn", {
+    "locale": "es",
+    mode: "range",
+    dateFormat: "d/M",
+    onOpen: function() {
+        document.querySelector("#dateRangeFilterBtn").removeAttribute('value');
+    },
+    onValueUpdate: function(selectedDates, dateStr) {
+        const filterPaymentsByDateBtn = document.querySelector("#dateRangeFilterBtn");
+
+        filterPaymentsByDateBtn.querySelector('span').innerText = dateStr;
+
+        filterByDateRange(selectedDates);
+        selectedDates.length && filterPaymentsByDateBtn.classList.add('filter--active');
+    },
+});
+
+//cleaning selection
+document.querySelector('.clear_button').addEventListener('click', function() {
+    const filterPaymentsByDateBtn = document.querySelector("#dateRangeFilterBtn");
+
+    filterPaymentsByDateBtn.querySelector('span').innerText = 'Fechas';
+    filterPaymentsByDateBtn.classList.remove('filter--active');
+    filterPaymentsByDateBtn.flatpickr().clear();
+    renderPayments(payments);
+  });
+
+  //search on payments
+  const searchInput = document.querySelector('#searchInput');
+  
+  const flattenObj = (obj, parent, res = {}) => {
+        for(let key in obj){
+            let propName = parent ? parent + '_' + key : key;
+            if(typeof obj[key] == 'object'){
+                flattenObj(obj[key], propName, res);
+            } else {
+                res[propName] = obj[key];
+            }
+        }
+        return res;
+    }
+
+    const searchOnPayments = () => {
+        const params = searchInput.value.toLowerCase();
+        const toRender = payments.filter(payment => Object.values(flattenObj(payment)).some(p => String(p).toLowerCase().includes(params)));
+        
+        toRender.length > 0
+        ? renderPayments(toRender)
+        : grid.innerHTML = 'No hay pagos que coincidan con la búsqueda';
+    }
+
+    searchInput.addEventListener('input', searchOnPayments);
+
+</script>
 @endsection
