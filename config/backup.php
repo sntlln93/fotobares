d5ddf1aa2a9303b95225bb40d5e62ca495354b9a
Matias Santillan <sntlln.93@gmail.com>
2022-09-27 06:31:18 -0300

    added files to weekly backup

--- config/backup.php
+++ config/backup.php
@@ -17,7 +17,7 @@ return [
                 /*
                  * The list of directories and files that will be included in the backup.
                  */
-                'include' => [],
+                'include' => [base_path('storage/app/public')],
 
                 /*
                  * These directories and files will be excluded from the backup.
